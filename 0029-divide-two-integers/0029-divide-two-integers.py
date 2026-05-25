48b4b09c6c182073708e7b91361122b6f0466bed
GauriOvhal <gaurimovhal@gmail.com>
2026-05-25 20:17:23 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.86%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def divide(self, dividend, divisor):
 
-        # Handle overflow case
         if dividend == -2147483648 and divisor == -1:
             return 2147483647
 
-        # Determine sign
         sign = -1 if (dividend < 0) ^ (divisor < 0) else 1
 
-        # Convert to positive
         dividend = abs(dividend)
         divisor = abs(divisor)
 
@@ -19,7 +16,6 @@ class Solution(object):
             temp = divisor
             multiple = 1
 
-            # Keep doubling
             while dividend >= (temp << 1):
                 temp <<= 1
                 multiple <<= 1
