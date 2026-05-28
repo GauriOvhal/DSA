7ebf9278510316d7b5cc37a270816a14e006d01d
GauriOvhal <gaurimovhal@gmail.com>
2026-05-28 19:28:11 +0530

    Time: 23 ms (2.69%), Space: 12.4 MB (53.07%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def hammingWeight(self, n):
+
+        binary_n = self.decToBinary(n)
+
+        count = 0
+        for ch in str(binary_n):
+            digit = int(ch)
+            if digit == 1:
+                count += 1
+
+        return count
+
+    def decToBinary(self , n):
+        if n == "0":
+            return 0
+
+        isNegative = n < 0
+        n = abs(n)
+
+        remainders_list = []
+
+        while n > 0:
+            remainder = n % 2
+            remainders_list.append(str(remainder))
+            n =  n // 2
+
+        remainders_list.reverse()
+        binary_string = "".join(remainders_list)
+
+        return "-"+binary_string if isNegative else binary_string
+        
